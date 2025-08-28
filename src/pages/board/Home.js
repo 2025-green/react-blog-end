b6d50aae4612521f4a4b739d373a4c8cf2a291b1
getinthere <getinthere@naver.com>
2025-08-28 11:47:02 +0900

    리엑트 블로그 완료

--- src/pages/board/Home.js
+++ src/pages/board/Home.js
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Form, FormControl, Pagination } from "react-bootstrap";
 import BoardItem from "../../components/BoardItem";
+import axios from "axios";
 
 const Home = () => {
   const [page, setPage] = useState(0);
   const [keyword, setKeyword] = useState("");
-  const [model, setModel] = useState({});
+  const [model, setModel] = useState({
+    totalPage: undefined,
+    number: 0,
+    isFirst: true,
+    isLast: false,
+    boards: [],
+  });
 
-  async function apiHome() {}
+  useEffect(() => {
+    apiHome();
+  }, [page, keyword]);
 
-  function changeValue(e) {}
+  async function apiHome() {
+    let response = await axios({
+      method: "get",
+      url: `http://localhost:8080?page=${page}&keyword=${keyword}`,
+    });
+
+    console.log(response.data.body.boards);
+    setModel(response.data.body);
+  }
+
+  function prev() {
+    setPage(page - 1);
+  }
+  function next() {
+    setPage(page + 1);
+  }
+
+  function changeValue(e) {
+    setKeyword(e.target.value);
+  }
 
   return (
     <div>
@@ -24,16 +52,18 @@ const Home = () => {
         />
       </Form>
 
-      <BoardItem key={1} id={1} title={"제목1"} page={0} />
+      {model.boards.map((board) => (
+        <BoardItem key={board.id} id={board.id} title={board.title} page={0} />
+      ))}
 
       <br />
       <div className='d-flex justify-content-center'>
         <Pagination>
-          <Pagination.Item onClick={""} disabled={false}>
+          <Pagination.Item onClick={prev} disabled={model.isFirst}>
             Prev
           </Pagination.Item>
 
-          <Pagination.Item onClick={""} disabled={false}>
+          <Pagination.Item onClick={next} disabled={model.isLast}>
             Next
           </Pagination.Item>
         </Pagination>
